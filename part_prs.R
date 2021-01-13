98d7c6ca867aaa09c86601e0f2f6ac2606209f08
mgeaghan <michael.p.geaghan@gmail.com>
2021-01-13 11:30:36 +1100

    simple plotting functions

--- part_prs.R
+++ part_prs.R
@@ -8,6 +8,7 @@ library(ggplot2)
 library(factoextra)
 
 # function definitions
+# loading data
 name_integer_cols <- function(df, column, integers, labels) {
   # replace integer values with labels in a specified column of a data.frame
   if(length(integers) != length(labels)) {
@@ -44,7 +45,7 @@ relabel_pheno_df <- function(pheno, character_sex = TRUE, character_phenotype = 
   return(new_pheno)
 }
 
-load_phenotypes <- function(fam_file, sep = " ", ...) {
+load_phenotypes <- function(fam_file, sep = " ", relabel_pheno = TRUE, ...) {
   # requires a PLINK fam file
   # produces a data frame with individual IDs as the row names, and two columns: sex and phenotype
   # sex: 1 = male; 2 = female
@@ -54,7 +55,9 @@ load_phenotypes <- function(fam_file, sep = " ", ...) {
   colnames(pheno) <- c("sex", "phenotype")
   pheno$sex[pheno$sex == 0] <- NA
   pheno$phenotype[pheno$phenotype %in% c(0, -9)] <- NA
-  pheno <- relabel_pheno_df(pheno, ...)
+  if (relabel_pheno) {
+    pheno <- relabel_pheno_df(pheno, ...)
+  }
   return(pheno)
 }
 
@@ -72,4 +75,40 @@ load_prs <- function(prs_file, pheno, sep = "\t", row.names = 1) {
   new_df <- melt(prs_long, id.vars = c("ID", "Pheno", "Sex"))
   colnames(new_df) <- c("ID", "Phenotype", "Sex", "PRS.Partition", "PRS")
   return(new_df)
+}
+
+# plotting PRS histograms
+prs_hist_cc <- function(df, title = "PRS Histogram - Case vs Control") {
+  return(ggplot(df, aes(x = PRS, fill = Phenotype)) +
+           geom_histogram(color = "black", position = "identity", alpha = 0.5) +
+           theme(legend.position = "bottom") +
+           ggtitle(title) +
+           theme(plot.title = element_text(hjust = 0.5)))
+}
+
+prs_hist_cc_partition <- function(df, partition, title = "PRS Histogram - Case vs Control") {
+  return(ggplot(df[df$PRS.Partition == partition,], aes(x = PRS, fill = Phenotype)) +
+           geom_histogram(color = "black", position = "identity", alpha = 0.5) +
+           theme(legend.position = "bottom") +
+           ggtitle(title) +
+           theme(plot.title = element_text(hjust = 0.5)))
+}
+
+prs_hist_partition_merge <- function(df, phenotype = NA, title = "PRS Histogram - Case vs Control") {
+  if(!is.na(phenotype)) {
+    new_df <- df[df$Phenotype == phenotype,]
+  } else {
+    new_df <- df
+  }
+  return(ggplot(new_df, aes(x = PRS, fill = PRS.Partition)) +
+           geom_histogram(color = "black", position = "identity", alpha = 0.5) +
+           theme(legend.position = "bottom") +
+           ggtitle(title) +
+           theme(plot.title = element_text(hjust = 0.5)))
+}
+
+prs_violin_cc_partition <- function(df, title = "PRS Violin Plots - Case vs Control") {
+  return(ggplot(df) +
+           geom_violin(aes(x = Phenotype, y = PRS, fill = Phenotype)) +
+           facet_wrap(~ PRS.Partition, ncol = 4, scales = "free_y"))
 }
